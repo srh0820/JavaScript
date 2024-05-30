d094cfbde795e5be7bce2622cd4c831b62b3e510
srh0820 <srh96820@gmail.com>
2024-05-30 11:33:47 +0900

    for in

--- arr_for.js
+++ arr_for.js
@@ -1,4 +1,4 @@
-console.log("한번만 실행");
+// console.log("한번만 실행");
 
 // for( 초기값선언; 조건; 조건을 거짓으로 만들어줄 연산식 )
 // i 가 0일 때부터; 5보다 작을 때까지; 하나씩 증가하면서;
@@ -6,8 +6,15 @@ console.log("한번만 실행");
 // 아직도 참이라면 실행하고 연산식실행 ... 그러다가 조건이 거짓이 되는 순간 for문 종료
 
 for( var i = 0; i < 5; i++ ){
-    console.log("다섯번 실행");
+    // console.log("다섯번 실행");
 }
+
+// for 추가대입식
+let meg = "";
+for( let i = 0; i < 10; i++ ){
+    meg += "\"열번\" 실행 \n";
+}
+// console.log(meg);
 
 
 // 연산식
@@ -23,7 +30,7 @@ let count = 10;
     ++count;         // 112
     count++;         // 112 (이 순간에 1은 추가로 못가지고 다음에 1을 가져갈 수 있음)
 
-    console.log(count)  // 113 (앞의 후치연산으로 인해 112가 됨)
+    // console.log(count)  // 113 (앞의 후치연산으로 인해 112가 됨)
 
 let multy = 10;
     multy = multy * 3;      // 30 (multy *= 3;)
@@ -41,3 +48,52 @@ let startjs = 10;
     startjs += "원";    // 20원 ("원"을 받아들이면서 문자로 변함)
     startjs += 100;     // 20원100
     startjs += 100;     // 20원100100
+
+
+const nav_data = [
+    {
+        gnb_li_a : "회사소개",
+        gnb_li_href : "./company.html"
+    },
+    {
+        gnb_li_a : "제품소개",
+        gnb_li_href : "./product.html"
+    },
+    {
+        gnb_li_a : "커뮤니티",
+        gnb_li_href : "./board.html"
+    },
+    {
+        gnb_li_a : "인트라넷",
+        gnb_li_href : "./intro.html"
+    }
+]
+// console.log(nav_data[1].gnb_li_href);
+// console.log(nav_data[1]["gnb_li_href"]);
+
+let mygnb = "";
+for ( let liea = 0; liea < 4; liea++ ){
+    mygnb += nav_data[liea].gnb_li_href;
+}
+console.log( mygnb );
+
+let mygnb2 = "";
+for ( let liea = 0; liea < nav_data.length; liea++ ){
+    mygnb2 += nav_data[liea].gnb_li_href;
+}
+console.log( mygnb2 );
+
+let mygnb3 = "";
+for ( let v in nav_data ){
+    console.log( "forin문 처리 : " , v );
+}
+
+let mygnb4 = "";
+for ( let v in nav_data ){;
+    mygnb4 += `${nav_data[v].gnb_li_href}\n`;
+}
+
+console.log( mygnb4 );
+
+// document.getElementsByIdClassName("gnb").length;
+// document.querySelectorAll(".gnb").length;
