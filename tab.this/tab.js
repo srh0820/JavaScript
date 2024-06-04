f2cb702bb0cd52d62ba5b2b1263fc8e4fcd5db3a
srh0820 <srh96820@gmail.com>
2024-06-04 11:38:41 +0900

    href 값까지 불러오기

--- tab.this/tab.js
+++ tab.this/tab.js
@@ -1,18 +1,57 @@
+// window.onload = function(){
+//     const tabbtn = document.querySelectorAll("#tab li");
+//     tabbtn.forEach(function(ele, idx){
+//         ele.addEventListener('click', function(){
+//             // this.classList = 'on';  초기화
+//             // this.classList += ' on'; 
+
+//             const siblings = this.parentElement.children;
+//             // 나의 부모의 자식들(object)을 siblings 라고 저장
+//             for (let sibling of siblings) {
+//                 sibling.classList.remove('on');
+//                 // sibling 에 있는 on을 제거
+//             }
+//             this.classList.add('on');
+//             // 나한테 다시 on 을 줌
+//         })
+//     })
+// }
+
 window.onload = function(){
     const tabbtn = document.querySelectorAll("#tab li");
+    // li들 저장
+  
     tabbtn.forEach(function(ele, idx){
+      // li들 각각 명령선언
+         
         ele.addEventListener('click', function(){
-            // this.classList = 'on';  초기화
-            // this.classList += ' on'; 
+            // li가 클릭되었을때 처리될 명령
+
+            const hrefValue = this.querySelector('a').getAttribute('href');
+            // this는 클릭한 li이기 때문에 그 안의 a태그를 먼저 선택하고 
+            // 그 속성 href의 값을 가져와서 hrefValue에 저장한다.
+            const targetele = document.querySelector(hrefValue);
+            // href값과 일치하는 객체 저장(아래 컨텐츠중 선택됨)
+        
+            const t_sibligns =  document.querySelectorAll(".content > div");
+            // 컨텐츠들 모두
+
+            t_sibligns.forEach(function(ele, idx){
+                ele.classList.add("d-none");
+                // 일단 모두 숨긴다.
+            })
+            targetele.classList.remove('d-none');
+            // 그리고 클릭a링크와 동일한 객체만 보여준다.
 
             const siblings = this.parentElement.children;
-            // 나의 부모의 자식들(object)을 siblings 라고 저장
-            for (let sibling of siblings) {
+            // 나의 부모의 자식들(html object) -> 노드객체 -> forEach사용불가
+            for (let sibling of siblings){
                 sibling.classList.remove('on');
-                // siblings 에 있는 on을 제거
+                // 일단 모든 li on빼서 빨강색없앤다.
             }
+                
             this.classList.add('on');
-            // 나한테 다시 on 을 줌
+            // 클랙 객체만 on클래스 추가해서 빨강색으로 노출한다.
         })
     })
 }
