d383fd437eb6208899683c675f601a904e24ba05
srh0820 <srh96820@gmail.com>
2024-06-26 11:17:14 +0900

    setInterval 이미지 지정해서 돌리기

--- setinterval_banner.js
+++ setinterval_banner.js
@@ -1,12 +1,26 @@
 // 자바스크립트 경로는 css 랑 다르다. 연결된 html 기준으로 처리해야한다.
 
 // 자바스크립트 표현법
-let count = 3;
-setInterval(function(){
-    count++;
-    count %= 5;
-    document.querySelector("#bannerimg").setAttribute("src",`/banner/${count}.png`)
-}, 1000);
+// let count = 3;
+// setInterval(function(){
+//     count++;
+//     count %= 5;
+//     document.querySelector("#bannerimg").setAttribute("src",`/banner/${count}.png`)
+// }, 1000);
 
 // jQuery 표현법
 // $("#banner img").attr("src", "/banner/1.png")
+
+const bannerdata = [
+    "/banner2/pokemon.png",
+    "/banner/0.png",
+    "/banner2/cantata.png"
+];
+
+let num = 0;
+document.querySelector("#bannerimg").setAttribute("src", bannerdata[num])  //  첫화면 "/banner2/pokemon.png" 
+setInterval(function(){
+    num++;
+    num %= bannerdata.length;
+    document.querySelector("#bannerimg").setAttribute("src", bannerdata[num])
+}, 1000)
